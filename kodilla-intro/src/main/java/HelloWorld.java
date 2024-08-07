470d30e5faabf1ab88dfa5bb7011a06c40a8efbc
Małgorzata Cybulska <cybulska.ma@gmail.com>
2024-08-07 11:51:58 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Małgorzata");
+        System.out.println(30);
+        System.out.println('C');
+    }
+}
