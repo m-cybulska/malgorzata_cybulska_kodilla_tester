e62d348fbccce59fcc92ddd43657482a73689ffa
Małgorzata Cybulska <cybulska.ma@gmail.com>
2024-08-07 11:55:31 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String favouriteColour = "Bottle green";
+        int luckyNumber = 24;
+        double eurovisionSongsMaxLength = 3.14;
+        boolean isSummerAwesome = true;
+        char yourFavouriteLetter = 'M';
+        String famousLastWords = "Can I pet that daawwg?!";
+
+        System.out.println(favouriteColour);
+        System.out.println(luckyNumber);
+        System.out.println(eurovisionSongsMaxLength);
+        System.out.println(isSummerAwesome);
+        System.out.println(yourFavouriteLetter);
+        System.out.println(famousLastWords);
+
+    }
+}
