96fc297d24f4b1dd06e9dad0215beb00f78c0ccd
Małgorzata Cybulska <cybulska.ma@gmail.com>
2024-09-04 13:42:13 +0200

    Task

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskExample.java
@@ -0,0 +1,6 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskExample {
+    Task task = new Task("Reading", 45);
+//    Task task = new Task("Cleaning", 30, "Kitchen");
+}
