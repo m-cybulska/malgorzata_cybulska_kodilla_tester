3eae0b2e85e60ec291b713020aa90e6d14a860f2
Małgorzata Cybulska <cybulska.ma@gmail.com>
2024-11-06 19:44:31 +0100

    Fizz Buzz

--- kodilla-cucumber/src/test/java/fizzBuzzHomework/IsNumberDivisible.java
+++ kodilla-cucumber/src/test/java/fizzBuzzHomework/IsNumberDivisible.java
@@ -11,38 +11,11 @@ public class IsNumberDivisible {
     private int number;
     private String message;
 
-    @Given("number 1")
-    public void number_1() {
-        this.number = 1;
+    @Given("number {int}")
+    public void number(int number) {
+        this.number = number;
     }
-    @Given("number 2")
-    public void number_2() {
-        this.number = 2;
-    }
-    @Given("number 3")
-    public void number_3() {
-        this.number = 3;
-    }
-    @Given("number 5")
-    public void number_5() {
-        this.number = 5;
-    }
-    @Given("number 6")
-    public void number_6() {
-        this.number = 6;
-    }
-    @Given("number 10")
-    public void number_10() {
-        this.number = 10;
-    }
-    @Given("number 15")
-    public void number_15() {
-        this.number = 15;
-    }
-    @Given("number 60")
-    public void number_60() {
-        this.number = 60;
-    }
+
     @When("I check the divisibility")
     public void i_check_the_divisibility() {
         this.message = checkNumber(this.number);
